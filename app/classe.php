11172667227cebf8afb14f1838c31c1afc8efb92
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-12 01:53:55 +0100

    GESTCOM V1.0.0

---
+++ app/classe.php
@@ -0,0 +1,19 @@
+<?php
+require dirname(__DIR__)."/app/autoloader.php";
+\App\autoloader::register();
+
+//Vendor Composer
+require dirname(__DIR__)."/vendor/autoload.php";
+
+use \Ovh\Api;
+
+$ak = "nhAYhfWMnOobopN1";
+$as = "quLnpFyrJO6fiOhBgsKMeHCAhr6Krdji";
+$endpoint = "https://eu.api.ovh.com/";
+$ck = "Vtc5O30QOJtL5MoJK2iXjRCfxnXU5yLU";
+$ovh = new Api($ak, $as, $endpoint, $ck);
+var_dump($ovh);
+
+
+
+
