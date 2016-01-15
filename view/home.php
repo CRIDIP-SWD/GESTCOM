b06210f6b8d9282f85f6ef1de75d3ada39a40a8e
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-15 18:51:40 +0100

    GESTCOM V1.0.0

--- view/home.php
+++ view/home.php
@@ -23,87 +23,62 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-8">
-                            <div class="chart-data-selector" id="salesSelectorWrapper">
-                                <h2>
-                                    Sales:
-                                    <strong>
-                                        <select class="form-control" id="salesSelector">
-                                            <option value="Porto Admin" selected>Porto Admin</option>
-                                            <option value="Porto Drupal" >Porto Drupal</option>
-                                            <option value="Porto Wordpress" >Porto Wordpress</option>
-                                        </select>
-                                    </strong>
-                                </h2>
+                            <div class="chart chart-md" id="flotBasic"></div>
+                            <script type="text/javascript">
 
-                                <div id="salesSelectorItems" class="chart-data-selector-items mt-sm">
-                                    <!-- Flot: Sales Porto Admin -->
-                                    <div class="chart chart-sm" data-sales-rel="Porto Admin" id="flotDashSales1" class="chart-active"></div>
-                                    <script>
+                                var flotBasicData = [{
+                                    data: [
+                                        [0, 170],
+                                        [1, 169],
+                                        [2, 173],
+                                        [3, 188],
+                                        [4, 147],
+                                        [5, 113],
+                                        [6, 128],
+                                        [7, 169],
+                                        [8, 173],
+                                        [9, 128],
+                                        [10, 128]
+                                    ],
+                                    label: "Series 1",
+                                    color: "#0088cc"
+                                }, {
+                                    data: [
+                                        [0, 115],
+                                        [1, 124],
+                                        [2, 114],
+                                        [3, 121],
+                                        [4, 115],
+                                        [5, 83],
+                                        [6, 102],
+                                        [7, 148],
+                                        [8, 147],
+                                        [9, 103],
+                                        [10, 113]
+                                    ],
+                                    label: "Series 2",
+                                    color: "#2baab1"
+                                }, {
+                                    data: [
+                                        [0, 70],
+                                        [1, 69],
+                                        [2, 73],
+                                        [3, 88],
+                                        [4, 47],
+                                        [5, 13],
+                                        [6, 28],
+                                        [7, 69],
+                                        [8, 73],
+                                        [9, 28],
+                                        [10, 28]
+                                    ],
+                                    label: "Series 3",
+                                    color: "#734ba9"
+                                }];
 
-                                        var flotDashSales1Data = [{
-                                            data: [
-                                                ["Jan", 140],
-                                                ["Feb", 240],
-                                                ["Mar", 190],
-                                                ["Apr", 140],
-                                                ["May", 180],
-                                                ["Jun", 320],
-                                                ["Jul", 270],
-                                                ["Aug", 180]
-                                            ],
-                                            color: "#0088cc"
-                                        }];
+                                // See: assets/javascripts/ui-elements/examples.charts.js for more settings.
 
-                                        // See: assets/javascripts/dashboard/examples.dashboard.js for more settings.
-
-                                    </script>
-
-                                    <!-- Flot: Sales Porto Drupal -->
-                                    <div class="chart chart-sm" data-sales-rel="Porto Drupal" id="flotDashSales2" class="chart-hidden"></div>
-                                    <script>
-
-                                        var flotDashSales2Data = [{
-                                            data: [
-                                                ["Jan", 240],
-                                                ["Feb", 240],
-                                                ["Mar", 290],
-                                                ["Apr", 540],
-                                                ["May", 480],
-                                                ["Jun", 220],
-                                                ["Jul", 170],
-                                                ["Aug", 190]
-                                            ],
-                                            color: "#2baab1"
-                                        }];
-
-                                        // See: assets/javascripts/dashboard/examples.dashboard.js for more settings.
-
-                                    </script>
-
-                                    <!-- Flot: Sales Porto Wordpress -->
-                                    <div class="chart chart-sm" data-sales-rel="Porto Wordpress" id="flotDashSales3" class="chart-hidden"></div>
-                                    <script>
-
-                                        var flotDashSales3Data = [{
-                                            data: [
-                                                ["Jan", 840],
-                                                ["Feb", 740],
-                                                ["Mar", 690],
-                                                ["Apr", 940],
-                                                ["May", 1180],
-                                                ["Jun", 820],
-                                                ["Jul", 570],
-                                                ["Aug", 780]
-                                            ],
-                                            color: "#734ba9"
-                                        }];
-
-                                        // See: assets/javascripts/dashboard/examples.dashboard.js for more settings.
-
-                                    </script>
-                                </div>
-
-                            </div>
+                            </script>
                         </div>
                         <div class="col-lg-4 text-center">
                             <h2 class="panel-title mt-md">Sales Goal</h2>
