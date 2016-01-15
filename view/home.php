ef43e9926f177d692788a079d527a5aacc3a79be
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-15 19:16:52 +0100

    GESTCOM V1.0.0

--- view/home.php
+++ view/home.php
@@ -12,7 +12,7 @@
                 <li><span>Tableau de Bord</span></li>
             </ol>
 
-            <!--<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>-->
+            <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
         </div>
     </header>
 
