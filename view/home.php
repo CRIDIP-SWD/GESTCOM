3dddb18e64dfed9103d879f5940a01593909b722
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-15 18:56:56 +0100

    GESTCOM V1.0.0

--- view/home.php
+++ view/home.php
@@ -22,7 +22,7 @@
             <section class="panel">
                 <div class="panel-body">
                     <div class="row">
-                        <div class="col-lg-8">
+                        <div class="col-lg-12">
                             <div class="chart chart-md" id="flotBasic"></div>
                             <script type="text/javascript">
 
@@ -48,18 +48,6 @@
                                 // See: assets/javascripts/ui-elements/examples.charts.js for more settings.
 
                             </script>
-                        </div>
-                        <div class="col-lg-4 text-center">
-                            <h2 class="panel-title mt-md">Sales Goal</h2>
-                            <div class="liquid-meter-wrapper liquid-meter-sm mt-lg">
-                                <div class="liquid-meter">
-                                    <meter min="0" max="100" value="35" id="meterSales"></meter>
-                                </div>
-                                <div class="liquid-meter-selector" id="meterSalesSel">
-                                    <a href="#" data-val="35" class="active">Monthly Goal</a>
-                                    <a href="#" data-val="28">Annual Goal</a>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
