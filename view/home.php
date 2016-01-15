9170ce592e5c121f7dcc1c9c1caab2f0bcda32f4
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-15 18:47:39 +0100

    GESTCOM V1.0.0

--- view/home.php
+++ view/home.php
@@ -1,6 +1,6 @@
 <section role="main" class="content-body">
     <header class="page-header">
-        <h2>Dashboard</h2>
+        <h2>Tableau de Bord</h2>
 
         <div class="right-wrapper pull-right">
             <ol class="breadcrumbs">
@@ -9,10 +9,10 @@
                         <i class="fa fa-home"></i>
                     </a>
                 </li>
-                <li><span>Dashboard</span></li>
+                <li><span>Tableau de Bord</span></li>
             </ol>
 
-            <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+            <!--<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>-->
         </div>
     </header>
 
