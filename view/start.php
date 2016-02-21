a8e9f8ffa40e4eb232ea158b18f70541528d0c2a
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-21 03:04:40 +0100

    GESTCOM V1.0.0

--- view/start.php
+++ view/start.php
@@ -54,12 +54,6 @@
         <!-- Center Wrap END -->
         <!-- END PAGE BASE CONTENT -->
         <!-- BEGIN FOOTER -->
-        <!-- BEGIN QUICK SIDEBAR TOGGLER -->
-        <button type="button" class="quick-sidebar-toggler" data-toggle="collapse">
-            <span class="sr-only">Toggle Quick Sidebar</span>
-            <i class="icon-logout"></i>
-        </button>
-        <!-- END QUICK SIDEBAR TOGGLER -->
         <p class="copyright">2015 © Metronic by keenthemes.
             <a href="http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" title="Purchase Metronic just for 27$ and get lifetime updates for free" target="_blank">Purchase Metronic!</a>
         </p>
