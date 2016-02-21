a9cd2f0ade2b9ca10cfca0fd8f1b80eb67ea7a9e
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-21 03:00:24 +0100

    GESTCOM V1.0.0

---
+++ view/start.php
@@ -0,0 +1,76 @@
+<!-- BEGIN CONTAINER -->
+<div class="page-content">
+    <div class="container">
+        <!-- BEGIN PAGE BASE CONTENT -->
+        <!-- Center Wrap BEGIN -->
+        <div class="center-wrap">
+            <div class="center-align">
+                <div class="center-body">
+                    <div class="row">
+                        <div class="col-sm-6 margin-bottom-30">
+                            <a class="webapp-btn" href="index.html">
+                                <h3>Application</h3>
+                                <p>Manage Applications</p>
+                            </a>
+                        </div>
+                        <div class="col-sm-6 margin-bottom-30">
+                            <a class="webapp-btn" href="dashboard_2.html">
+                                <h3>Reports</h3>
+                                <p>View Reports</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 sm-margin-bottom-30">
+                            <a class="webapp-btn" href="ecommerce_index.html">
+                                <h3>Templates</h3>
+                                <p>Add, edit templates</p>
+                            </a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a class="webapp-btn" href="dashboard_3.html">
+                                <h3>Settings</h3>
+                                <p>Manage your Projects</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Center Wrap END -->
+        <!-- END PAGE BASE CONTENT -->
+        <!-- BEGIN FOOTER -->
+        <!-- BEGIN QUICK SIDEBAR TOGGLER -->
+        <button type="button" class="quick-sidebar-toggler" data-toggle="collapse">
+            <span class="sr-only">Toggle Quick Sidebar</span>
+            <i class="icon-logout"></i>
+        </button>
+        <!-- END QUICK SIDEBAR TOGGLER -->
+        <p class="copyright">2015 © Metronic by keenthemes.
+            <a href="http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" title="Purchase Metronic just for 27$ and get lifetime updates for free" target="_blank">Purchase Metronic!</a>
+        </p>
+        <!-- END FOOTER -->
+    </div>
+</div>
+<!-- END CONTAINER -->
+<!--[if lt IE 9]>
+<script src="../assets/global/plugins/respond.min.js"></script>
+<script src="../assets/global/plugins/excanvas.min.js"></script>
+<![endif]-->
+<!-- BEGIN CORE PLUGINS -->
+<script src="../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="../assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+<script src="../assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="../assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="../assets/global/scripts/app.min.js" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="../assets/layouts/layout6/scripts/layout.min.js" type="text/javascript"></script>
+<script src="../assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
