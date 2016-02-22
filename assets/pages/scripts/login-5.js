a003ea068708d7e238bb827b4a4f361f24f91684
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-22 15:19:33 +0100

    GESTCOM V1.0.0

--- assets/pages/scripts/login-5.js
+++ assets/pages/scripts/login-5.js
@@ -49,7 +49,7 @@ var Login = function() {
                 form.submit(function(){
                    var pseudo       = $(this).find("input[name=username]").val();
                    var password     = $(this).find("input[name=password]").val();
-                   var action       = $(this).find("input[name=action]").val();
+                   var action       = $(this).find("button[name=action]").val();
 
                     $.post("../../../core/general/user.php", {pseudo: pseudo, password: password, action: action}, function (data) {
                        alert(data)
