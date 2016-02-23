019d31468293f86c49f3f41528b5db3c71bec9c7
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-23 11:25:12 +0100

    GESTCOM V1.0.0

--- assets/pages/scripts/custom.js
+++ assets/pages/scripts/custom.js
@@ -13,12 +13,14 @@ var Custom = function () {
         e.preventDefault();
         var $form = $(this);
         $form.find('button').html("<i class='fa fa-spinner fa-spin'></i> Connexion en cours");
-        $.post($form.attr('action'), $form.serializeArray())
+        $.post($form.attr('action'), $form.serializeArray(), "json")
             .done(function(jqxhr){
                 document.location.href(jqxhr.responseText);
             })
-            .fail(function(jqxhr){
-                toastr.error("Une erreur a eu lieu lors de la connexion", "Connexion");
+            .fail(function(jqxhr, data){
+                if(data.error == 'no'){
+
+                }
             })
             .always(function(){
                 $form.find('button').text("Connexion")
--- core/general/user.php
+++ core/general/user.php
@@ -13,6 +13,9 @@ if(!isset($_POST['username']) || empty($_POST['username']) || !isset($_POST['pas
         "password"  => $sha_pass
     ));
 
+    $t = array();
+    $t['error'] = "Formulaire Incorrect";
+
     if($fonction->isAjax())
     {
         if($user[0] == 1)
@@ -21,28 +24,30 @@ if(!isset($_POST['username']) || empty($_POST['username']) || !isset($_POST['pas
             $user_db = $DB->query("SELECT * FROM users WHERE username = :username", array("username" => $username));
             $_SESSION['user']['connect'] = 1;
             $_SESSION['user']['username'] = $username;
+            $t['error'] = "no";
 
             switch($user_db[0]->groupe)
             {
                 case 1:
-                    header("Location: ../../index.php?view=start");
+                    $t['retour'] = header("Location: ../../index.php?view=start");
                     break;
                 case 2:
-                    header("Location: ../../index.php?view=start");
+                    $t['retour'] = header("Location: ../../index.php?view=start");
                     break;
                 case 3:
-                    header("Location: ../../index.php?view=start");
+                    $t['retour'] = header("Location: ../../index.php?view=start");
                     break;
                 case 4:
-                    header("Location: ../../view/client/index.php");
+                    $t['retour'] = header("Location: ../../view/client/index.php");
                     break;
             }
 
         }else{
-            echo "ERROR !!!";
+            $t['errorLoginPass'] = "Connexion Echoué: Mauvais utilisateur ou mot de passe !!";
         }
     }else{
-        echo "Not Ajax embedded";
+        $t['errorChamps'] = "Veuillez saisir tout les champs requis !";
     }
+    echo json_encode($t);
 
 }
