274dbc525d14f7d1a82cbde82be7941eb3a56a10
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-23 14:58:03 +0100

    GESTCOM V1.0.0

--- view/collab/default.php
+++ view/collab/default.php
@@ -181,7 +181,7 @@ License: You must have a valid license purchased only from themeforest(the above
                     <!-- BEGIN USER PROFILE -->
                     <div class="btn-group-img btn-group">
                         <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                            <img src="../assets/layouts/layout5/img/avatar1.jpg" alt=""> </button>
+                            <img src="<?= $constante->getUrl(array(), false, true); ?>avatar/mmockelyn.jpg" alt=""> </button>
                         <ul class="dropdown-menu-v2" role="menu">
                             <li>
                                 <a href="page_user_profile_1.html">
