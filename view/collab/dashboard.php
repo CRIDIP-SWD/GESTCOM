8c068879f9c07d4d155b9f131dd78db4f14115b8
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-23 15:23:21 +0100

    GESTCOM V1.0.0

---
+++ view/collab/dashboard.php
@@ -0,0 +1,33 @@
+<div class="page-fixed-main-content">
+    <!-- BEGIN PAGE BASE CONTENT -->
+    <div class="note note-info">
+        <p> A black page template with a minimal dependency assets to use as a base for any custom page you create </p>
+    </div>
+    <!-- END PAGE BASE CONTENT -->
+</div>
+<?php include $constante->getUrl(array(), false, false)."view/footer.php"; ?>
+</div>
+</div>
+<!-- END CONTAINER -->
+<?php include $constante->getUrl(array(), false, false)."view/sidebar_left.php"; ?>
+<!--[if lt IE 9]>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/respond.min.js"></script>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/excanvas.min.js"></script>
+<![endif]-->
+<!-- BEGIN CORE PLUGINS -->
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/jquery.min.js" type="text/javascript"></script>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/js.cookie.min.js" type="text/javascript"></script>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="<?= $constante->getUrl(array('global/')); ?>plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="<?= $constante->getUrl(array('global/')); ?>scripts/app.min.js" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="<?= $constante->getUrl(array('layouts/')); ?>layout6/scripts/layout.min.js" type="text/javascript"></script>
+<script src="<?= $constante->getUrl(array('layouts/')); ?>global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
