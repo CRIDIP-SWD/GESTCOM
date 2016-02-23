45350b9d630c18fa864126484cbde6871f1d149e
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-23 15:26:39 +0100

    GESTCOM V1.0.0

--- view/collab/dashboard.php
+++ view/collab/dashboard.php
@@ -1,7 +1,7 @@
 <div class="page-fixed-main-content">
     <!-- BEGIN PAGE BASE CONTENT -->
     <div class="note note-info">
-        <p> A black page template with a minimal dependency assets to use as a base for any custom page you create </p>
+        <p> A black page template with a minimal dependency assets to use as a base for any custom page you create Despaire</p>
     </div>
     <!-- END PAGE BASE CONTENT -->
 </div>
