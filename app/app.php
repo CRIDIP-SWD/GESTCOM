e3ce1144a1265499c2dac903386f094fc48cbf37
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-25 00:56:56 +0100

    GESTCOM V1.0.0

--- app/app.php
+++ app/app.php
@@ -343,9 +343,9 @@ class fonction extends app
 class DB extends app{
 
     private $host = "localhost";
-    private $username = "root";
+    private $username = "gc";
     private $password = "1992maxime";
-    private $database = "gestcom";
+    private $database = "gc";
     private $db;
 
     public function __construct($host = null, $username = null, $password = null, $database = null)
