1ea1a612bcbaa5092f8de2c84cfbc981032362ef
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-24 16:55:01 +0100

    GESTCOM V1.0.0

--- view/collab/profil.php
+++ view/collab/profil.php
@@ -11,7 +11,7 @@
         </div>
         <div class="col-md-9">
             <div class="well text-right">
-                <a class="btn btn-danger">Test</a>
+                <a class="btn btn-primary btn-lg btn-circle" data-toggle="modal" href="#edit-profil"><i class="fa fa-edit"></i></a>
             </div>
         </div>
     </div>
