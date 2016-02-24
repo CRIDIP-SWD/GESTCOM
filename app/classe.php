f343d0abc2776c73aef5afee1929e9ee2a80341e
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-25 00:43:29 +0100

    GESTCOM V1.0.0

--- app/classe.php
+++ app/classe.php
@@ -2,8 +2,6 @@
 require dirname(__DIR__)."/app/autoloader.php";
 \App\autoloader::register();
 
-//Vendor Composer
-require dirname(__DIR__)."/vendor/autoload.php";
 
 use App\administration\configuration;
 use App\administration\users;
@@ -14,7 +12,6 @@ use App\date_format;
 use App\DB;
 use App\fonction;
 use App\IP_API;
-use \Ovh\Api;
 
 $app = new app();
 $constante = new constante();
