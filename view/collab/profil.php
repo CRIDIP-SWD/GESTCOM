042dce630bc69f7ecbdf6cd25202348dc2b0f852
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-25 19:37:54 +0100

    GESTCOM V1.0.0

--- view/collab/profil.php
+++ view/collab/profil.php
@@ -149,6 +149,72 @@
     </div>
     <!-- /.modal-dialog -->
 </div>
+<div class="modal fade bs-modal-lg" id="active-totp" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header bg-blue">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title"><i class="fa fa-edit"></i> Edition du profil</h4>
+            </div>
+            <form id="form-edit-profil" class="form-horizontal" action="<?= $constante->getUrl(array(), false, false); ?>core/general/user.php" method="post">
+                <input type="hidden" name="iduser" value="<?= $info_user[0]->iduser; ?>">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="profil">Nom</label>
+                        <div class="col-md-9">
+                            <input type="text" id="profil" class="form-control" name="nom_user" value="<?= $info_user[0]->nom_user; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="profil">Prénom</label>
+                        <div class="col-md-9">
+                            <input type="text" id="profil" class="form-control" name="prenom_user" value="<?= $info_user[0]->prenom_user; ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn dark btn-outline" data-dismiss="modal">Fermer</button>
+                    <button type="submit" class="btn green" name="action" value="edit-profil">Save changes</button>
+                </div>
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<div class="modal fade bs-modal-lg" id="desactive-totp" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header bg-blue">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title"><i class="fa fa-edit"></i> Edition du profil</h4>
+            </div>
+            <form id="form-edit-profil" class="form-horizontal" action="<?= $constante->getUrl(array(), false, false); ?>core/general/user.php" method="post">
+                <input type="hidden" name="iduser" value="<?= $info_user[0]->iduser; ?>">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="profil">Nom</label>
+                        <div class="col-md-9">
+                            <input type="text" id="profil" class="form-control" name="nom_user" value="<?= $info_user[0]->nom_user; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="profil">Prénom</label>
+                        <div class="col-md-9">
+                            <input type="text" id="profil" class="form-control" name="prenom_user" value="<?= $info_user[0]->prenom_user; ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn dark btn-outline" data-dismiss="modal">Fermer</button>
+                    <button type="submit" class="btn green" name="action" value="edit-profil">Save changes</button>
+                </div>
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
 <?php include $constante->getUrl(array(''), false, false)."view/footer.php"; ?>
 </div>
 </div>
