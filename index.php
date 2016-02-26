2d29134fbcde01f459a2b282cc394ce3952aab3b
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-26 17:24:13 +0100

    GESTCOM V1.0.0

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
-
-use App\Router\Router;
-
+session_start();
 require "application/classe.php";
+App\autoloader::register();
 
-$router = new Router($_GET['url']);
-$router->get("/", function($id){ include('view/index.php');});
-$router->get("/posts/:id", function($id){echo "Voici l'article $id";});
-$router->run();
+if(isset($_GET['view']))
+{
+    $view = $_GET['view'];
+}else{
+    $view = "index";
+}
+
+ob_start();
+if($view === 'starter'){include "view/index.php";}
+
+$content = ob_get_clean();
+if($view === 'login'){
+    require "view/login.php";
+}else{
+    require "view/default.php";
+}
+
+
---
+++ view/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SWD
+ * Date: 26/02/2016
+ * Time: 17:24
+ */
