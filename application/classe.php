b37d14c6cb91050bd606ddc6f33faa2fb44558d9
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-26 15:54:04 +0100

    GESTCOM V1.0.0

---
+++ application/classe.php
@@ -0,0 +1,31 @@
+<?php
+use App\app;
+use App\constante;
+use App\date_format;
+use App\DB;
+use App\fonction;
+use App\ssh2;
+
+
+require dirname(__DIR__)."/application/autoloader.php";
+\App\autoloader::register();
+
+//VENDOR COMPOSER
+//include dirname(__DIR__)."/vendor/autoload.php";
+
+
+//NAMESPACE APP
+$app = new app();
+$constante = new constante();
+$date_format = new date_format();
+$fonction = new fonction();
+$DB = new DB();
+$ssh2 = new ssh2();
+
+
+
+//COMPOSER
+
+
+
+
