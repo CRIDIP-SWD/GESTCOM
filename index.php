d08c5605c4a13e96edff5b1d85c3687de525267b
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-26 17:19:12 +0100

    GESTCOM V1.0.0

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ use App\Router\Router;
 require "application/classe.php";
 
 $router = new Router($_GET['url']);
-$router->get("/", function($id){ include('view/index.php?id=$id');});
+$router->get("/", function($id){ include('view/index.php');});
 $router->get("/posts/:id", function($id){echo "Voici l'article $id";});
 $router->run();
