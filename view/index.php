aab906f2713eddd6c5b9a7ed29649ffcdf97a3a0
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-26 17:13:23 +0100

    GESTCOM V1.0.0

---
+++ view/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+</head>
+<body>
+    <h2>Bienvenue les gents sur ma Homepage</h2>
+</body>
+</html>
