5ceab7b0443bb239512f9a0bf29268aa1e95cf85
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-26 15:33:01 +0100

    GESTCOM V1.0.0

--- core/collab/ajax/message.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../../../app/classe.php";
-
-$iduser = $_GET['iduser'];
-
-$count = $DB->count("SELECT count(idinbox) FROM collab_inbox WHERE destinataire = :iduser AND lu = 0", array("iduser" => $iduser));
-
-echo $count;
