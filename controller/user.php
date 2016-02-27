2a72df951611349d0b5225cd5d0c123dfc449be5
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 01:12:12 +0100

    GESTCOM V1.0.0

--- controller/user.php
+++ controller/user.php
@@ -7,7 +7,7 @@ if(isset($_POST['action']) && $_POST['action'] == 'login')
 
     if(!isset($username) || empty($username) && !isset($password) || empty($password)){
         $text = "Au moins un des champs requis n'est pas remplie !";
-        $fonction->redirect("login", "", "", "warning", "login", "$text")
+        $fonction->redirect("login", "", "", "warning", "login", "$text");
     }else{
         $user_co = $DB->count("SELECT COUNT(iduser) FROM users WHERE username = :username", array("username" => $username));
 
