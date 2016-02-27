65dfe023787c56086949af4f07402f39b32bae5a
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 03:55:33 +0100

    GESTCOM V1.0.0

--- view/index.php
+++ view/index.php
@@ -1,14 +1,34 @@
-<?php
-$id = $_GET['id'];
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Accueil</title>
-</head>
-<body>
-    <h2>Bienvenue les gents sur ma Homepage</h2>
-    <p>L'affichage par get est de : <?= $id; ?></p>
-</body>
-</html>
+<div class="center-wrap">
+    <div class="center-align">
+        <div class="center-body">
+            <div class="row">
+                <div class="col-sm-6 margin-bottom-30">
+                    <a href="index.html" class="webapp-btn">
+                        <h3>Application</h3>
+                        <p>Manage Applications</p>
+                    </a>
+                </div>
+                <div class="col-sm-6 margin-bottom-30">
+                    <a href="dashboard_2.html" class="webapp-btn">
+                        <h3>Reports</h3>
+                        <p>View Reports</p>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 sm-margin-bottom-30">
+                    <a href="ecommerce_index.html" class="webapp-btn">
+                        <h3>Templates</h3>
+                        <p>Add, edit templates</p>
+                    </a>
+                </div>
+                <div class="col-sm-6">
+                    <a href="dashboard_3.html" class="webapp-btn">
+                        <h3>Settings</h3>
+                        <p>Manage your Projects</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
