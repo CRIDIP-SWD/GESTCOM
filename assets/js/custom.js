a0466b12f5aedf4078329b573c97e308de688888
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 03:21:31 +0100

    GESTCOM V1.0.0

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,16 +16,12 @@
             data: data,
             success: function(data, jqxhr){
                 if(data == 0){
-                    stap.remove();
-                    stap.html('<i id="stap" class="busy"></i><span>Hors Ligne</span><i class="fa fa-angle-down"></i>');
-                    a.remove();
-                    a.html('<li><a href="controller/user.ajax.php?action=connector&connect=2" id="connector"><i class="online"></i><span>En Ligne</span></a></li>');
+                    stap.replaceWith('<i id="stap" class="busy"></i><span>Hors Ligne</span><i class="fa fa-angle-down"></i>');
+                    a.replaceWith('<li><a href="controller/user.ajax.php?action=connector&connect=2" id="connector"><i class="online"></i><span>En Ligne</span></a></li>');
                 }
                 if(data == 2){
-                    stap.remove();
-                    stap.html('<i id="stap" class="online"></i><span>En Ligne</span><i class="fa fa-angle-down"></i>');
-                    a.remove();
-                    a.html('<li><a href="controller/user.ajax.php?action=connector&connect=0" id="connector"><i class="busy"></i><span>Hors Ligne</span></a></li>');
+                    stap.replaceWith('<i id="stap" class="online"></i><span>En Ligne</span><i class="fa fa-angle-down"></i>');
+                    a.replaceWith('<li><a href="controller/user.ajax.php?action=connector&connect=0" id="connector"><i class="busy"></i><span>Hors Ligne</span></a></li>');
                 }
 
             },
