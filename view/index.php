01fc18a0de85fc86f7b3077abebfc10b463100bc
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 03:58:13 +0100

    GESTCOM V1.0.0

--- view/index.php
+++ view/index.php
@@ -1,34 +1,5 @@
-<div class="center-wrap">
-    <div class="center-align">
-        <div class="center-body">
-            <div class="row">
-                <div class="col-sm-6 margin-bottom-30">
-                    <a href="index.html" class="webapp-btn">
-                        <h3>Application</h3>
-                        <p>Manage Applications</p>
-                    </a>
-                </div>
-                <div class="col-sm-6 margin-bottom-30">
-                    <a href="dashboard_2.html" class="webapp-btn">
-                        <h3>Reports</h3>
-                        <p>View Reports</p>
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 sm-margin-bottom-30">
-                    <a href="ecommerce_index.html" class="webapp-btn">
-                        <h3>Templates</h3>
-                        <p>Add, edit templates</p>
-                    </a>
-                </div>
-                <div class="col-sm-6">
-                    <a href="dashboard_3.html" class="webapp-btn">
-                        <h3>Settings</h3>
-                        <p>Manage your Projects</p>
-                    </a>
-                </div>
-            </div>
-        </div>
+<div class="row">
+    <div class="col-md-12 text-center">
+        <a class="btn btn-danger">Test</a>
     </div>
 </div>
