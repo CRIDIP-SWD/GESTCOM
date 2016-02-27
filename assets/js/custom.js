4b4f76fe98e5ea374b195acb7a392c7b051f4fd6
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 02:46:16 +0100

    GESTCOM V1.0.0

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,9 +12,7 @@
             url: url,
             data: data,
             success: function(data){
-                if(data["connect"] == 0){toastr.info("Vous êtes Maintenant <strong>Hors Ligne</strong> !");}
-                if(data["connect"] == 1){toastr.info("Vous êtes Maintenant <strong>Absent</strong> !");}
-                if(data["connect"] == 2){toastr.info("Vous êtes Maintenant <strong>En Ligne</strong> !");}
+                alert(data);
             },
             error: function(jqxhr){
                 alert(jqxhr);
