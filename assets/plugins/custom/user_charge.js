1833cf40d038fa38157bbb4be101f771e49bb9eb
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 19:11:58 +0100

    GESTCOM V1.0.0

--- assets/plugins/custom/user_charge.js
+++ assets/plugins/custom/user_charge.js
@@ -2,7 +2,7 @@
  * Created by Maxime on 27/02/2016.
  */
 (function($){
-    setInterval(
+    function ajax(){
         /** VERIFICATION A LA CONNEXION DE L'UTILISATEUR **/
         $.ajax({
             url: "../../../controller/user.ajax.php?action=check-message",
@@ -18,26 +18,28 @@
             error:function(jqxhr){
                 console.log(jqxhr.responseText);
             }
-        }),
-        1000);
-    setInterval($.ajax({
-        url: "../../../controller/user.ajax.php?action=check-notif",
-        dataType: "json",
-        type: "GET",
-        success: function(data){
-            if(data >= 1){
-                $('#count_notif').replaceWith('<span id="count_notif" class="badge badge-danger badge-header">'+data+'</span>');
-                $('#count_notif_title').replaceWith('<p class="pull-left" id="count_notif_title">'+data+' Notification en attente</p>');
-                toastr.info("Vous avez une nouvelle notification !");
-            }else{
-                $('#count_notif').remove();
-                $('#count_notif_title').replaceWith('<p class="pull-left" id="count_notif_title">'+data+' Notification en attente</p>');
+        });
+        $.ajax({
+            url: "../../../controller/user.ajax.php?action=check-notif",
+            dataType: "json",
+            type: "GET",
+            success: function(data){
+                if(data >= 1){
+                    $('#count_notif').replaceWith('<span id="count_notif" class="badge badge-danger badge-header">'+data+'</span>');
+                    $('#count_notif_title').replaceWith('<p class="pull-left" id="count_notif_title">'+data+' Notification en attente</p>');
+                    toastr.info("Vous avez une nouvelle notification !");
+                }else{
+                    $('#count_notif').remove();
+                    $('#count_notif_title').replaceWith('<p class="pull-left" id="count_notif_title">'+data+' Notification en attente</p>');
 
+                }
+            },
+            error: function(jqxhr){
+                console.log(jqxhr.responseText);
             }
-        },
-        error: function(jqxhr){
-            console.log(jqxhr.responseText);
-        }
-    }), 1000);
+        });
+    }
+    setInterval(ajax(), 4000);
+
 
 })(jQuery);
