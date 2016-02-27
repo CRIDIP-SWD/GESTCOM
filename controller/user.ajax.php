0755680b1d9ed885f09ed690da89aea557f84d32
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 03:07:35 +0100

    GESTCOM V1.0.0

--- controller/user.ajax.php
+++ controller/user.ajax.php
@@ -1,21 +1,39 @@
 <?php
-if(isset($_GET['action']) && $_GET['action'] == 'connector')
-{
-    session_start();
-    require "../application/classe.php";
-    $connect = $_GET['connect'];
-    $username = $user->username;
-
-    $user_u = $DB->execute("UPDATE users SET connect = :connect WHERE username = :username", array(
-        "connect"   => $connect,
-        "username"  => $username
-    ));
-    if($fonction->is_ajax()){
-        $return = $connect;
-        $return["json"] =json_encode($return);
-        echo json_encode($return);
-    }else{
-        $fonction->redirect("dashboard", "","", "info", "connector", "Changement du Statut !");
+require "../application/classe.php";
+if($fonction->is_ajax()){
+    if(isset($_GET['action']) && $_GET['action'] == 'connector')
+    {
+        $connect = $_GET['connect'];
+        echo json_encode($connect);
     }
+}
+?>
+
+<?php
+if (is_ajax()) {
+    if (isset($_POST["action"]) && !empty($_POST["action"])) { //Checks if action value exists
+        $action = $_POST["action"];
+        switch($action) { //Switch case for value of action
+            case "test": test_function(); break;
+        }
+    }
+}
+
+//Function to check if the request is an AJAX request
+function is_ajax() {
+    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+}
+
+function test_function(){
+    $return = $_POST;
+
+    //Do what you need to do with the info. The following are some examples.
+    //if ($return["favorite_beverage"] == ""){
+    // $return["favorite_beverage"] = "Coke";
+    //}
+    //$return["favorite_restaurant"] = "McDonald's";
+
+    $return["json"] = json_encode($return);
+    echo json_encode($return);
 }
 ?>
