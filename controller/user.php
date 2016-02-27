eff5dcd4e0f1bf96e4493e8b882190bf93081938
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 01:28:38 +0100

    GESTCOM V1.0.0

--- controller/user.php
+++ controller/user.php
@@ -5,12 +5,30 @@ if(isset($_POST['action']) && $_POST['action'] == 'login')
     $username = $_GET['username'];
     $password = $_GET['password'];
 
-    if(!isset($username) && empty($username) && !isset($password) && empty($password)){
-        $text = "Au moins un des champs requis n'est pas remplie !";
-        $fonction->redirect("login", "", "", "warning", "login", "$text");
-    }else{
+    if(isset($_POST['pseudo']) && !empty($_POST['pseudo']) && isset($_POST['password']) && !empty($_POST['password'])){
         $user_co = $DB->count("SELECT COUNT(iduser) FROM users WHERE username = :username", array("username" => $username));
 
-        var_dump($user_co);
+        if($user_co == 1){
+            session_start();
+            $_SESSION['account']['active'] = 1;
+            $_SESSION['account']['username'] = $username;
+
+            $user_u = $DB->execute("UPDATE users SET connect = 2, last_connect = :last_connect WHERE username = :username", array(
+                "username"      => $username,
+                "last_connect"  => $date_format->format_strt(date("d-m-Y H:i:s"))
+            ));
+
+            if($user_u == 1){
+                $fonction->redirect("dashboard", "", "","", "","");
+            }elseif($user_u == 0){
+                $text = "Aucun couple Nom d'utilisateur / Mot de Passe correspondant.";
+                $fonction->redirect("login", "","","error", "login", $text);
+            }else{
+                $fonction->redirect("error", "","","code", "USR1", "");
+            }
+        }
+    }else{
+        $text = "Au moins un des champs requis n'est pas remplie !";
+        $fonction->redirect("login", "", "", "warning", "login", $text);
     }
 }
