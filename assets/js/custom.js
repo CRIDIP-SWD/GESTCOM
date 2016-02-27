659de4e6024f8d433c9593eb0de37d4643727b63
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 03:01:32 +0100

    GESTCOM V1.0.0

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,8 +12,8 @@
             dataType: "json",
             url: url,
             data: data,
-            success: function(data){
-                alert(data);
+            success: function(data, jqxhr){
+                alert(jqxhr);
             },
             error: function(jqxhr){
                 alert(jqxhr);
