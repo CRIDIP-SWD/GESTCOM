51eb4205063ea3dfd5fa018e5b52cda66188f28a
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 04:06:44 +0100

    GESTCOM V1.0.0

--- view/index.php
+++ view/index.php
@@ -1,5 +1,8 @@
 <div class="row">
     <div class="col-md-12 text-center">
-        <a class="btn btn-danger">Test</a>
+        <a class="btn-box" style="min-height: 350px; height: 280px;">
+            <i class="fa fa-euro" style="font-size: 100px; position:relative; top: 150px;"></i>
+            <div style="font-size: 35px;">Collaboration</div>
+        </a>
     </div>
 </div>
