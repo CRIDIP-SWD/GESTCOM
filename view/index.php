9fa6ef0faa77d9ad28dfc2172a297b09bb11fcd8
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-27 04:08:58 +0100

    GESTCOM V1.0.0

--- view/index.php
+++ view/index.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-12 text-center">
+    <div class="col-md-6 text-center">
         <a class="btn-box">
             <i class="fa fa-euro"></i>
             <div style="font-size: 35px;">Collaboration</div>
