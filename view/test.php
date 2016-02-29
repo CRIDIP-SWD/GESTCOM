10af9a664dd156bbaa1dbf9e598dde42c92aa4e3
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-29 14:49:54 +0100

    GESTCOM V1.0.0

--- view/test.php
+++ view/test.php
@@ -1,5 +1,5 @@
 <?php
-$encrypt = new \App\noctus\encrypt("test", "test");
+$encrypt = new \App\noctus\encrypt("mmockelyn", "1992maxime");
 $sys = $encrypt->encrypt();
 ?>
 <pre><?php var_dump($sys); ?></pre>
