1c8f1dc73a736b7a5d93bb535e9b7175e6abc401
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-29 14:52:11 +0100

    GESTCOM V1.0.0

--- view/test.php
+++ view/test.php
@@ -1,5 +1,14 @@
 <?php
 $encrypt = new \App\noctus\encrypt("mmockelyn", "1992maxime");
-$sys = $encrypt->encrypt();
+$en_user = $encrypt->encrypt();
+$decrypt = new \App\noctus\decrypt($en_user, "mmockelyn", "1992maxime");
+$de_user = $decrypt->decrypt();
 ?>
-<pre><?php var_dump($sys); ?></pre>
+<h2>Encrypt User</h2>
+<pre><?php var_dump($en_user); ?></pre>
+
+<h2>Decrypt user</h2>
+<?php
+if($de_user == true){echo "<pre>OK</pre>";}else{echo "<pre>Erreur</pre>";}
+?>
+
