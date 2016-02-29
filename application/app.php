05ada40be977103389d9d55f10912ffc3ded0539
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-29 15:25:37 +0100

    GESTCOM V1.0.0

--- application/app.php
+++ application/app.php
@@ -327,7 +327,7 @@ class fonction extends app
     public function redirect($view = null, $sub = null, $data = null, $type = null, $service = null, $text = null){
         $constante = new constante();
 
-        if(empty($view)){
+        /*if(empty($view)){
             header("Location: ".$constante->getUrl(array(), false)."index.php");
         }
         if(!empty($view)){
@@ -350,9 +350,14 @@ class fonction extends app
         }
         if(!empty($type)){
             header("Location: ".$constante->getUrl(array(), false)."index.php?view=".$view."&sub=".$sub."&data=".$data."&".$type."=".$service."&text=".$text);
-        }
+        }*/
 
+        if(!empty($view)){$redirect = "?view=".$view;}
+        if(!empty($sub)){$redirect = "&sub=".$sub;}
+        if(!empty($data)){$redirect = "&data=".$data;}
+        if(!empty($type)){$redirect = "&".$type."=".$service."&text".$text;}
 
+        header("Location: ".$constante->getUrl(array(), false).$redirect);
 
     }
 
