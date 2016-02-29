17954cfe687ad0ab2ecc943f27134f967cb1b33a
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-29 15:30:40 +0100

    GESTCOM V1.0.0

--- controller/user.php
+++ controller/user.php
@@ -14,7 +14,7 @@ if(isset($_POST['action']) && $_POST['action'] == 'login')
 
         $user_co = $DB->count("SELECT COUNT(iduser) FROM users WHERE username = :username AND password = :password", array(
             "username" => $username,
-            "password" => $pass_de
+            "password" => $pass_en
         ));
 
 
