fcc86b8ff04cb3bca71a5e14c67e811cc305eab4
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-29 16:11:40 +0100

    GESTCOM V1.0.0

--- assets/plugins/custom/user_time.js
+++ assets/plugins/custom/user_time.js
@@ -1,14 +1,22 @@
 /**
  * Created by SWD on 29/02/2016.
  */
-$(document).ready(function(){
-    function ajax(){
-        /** VERIFICATION A LA CONNEXION DE L'UTILISATEUR **/
-        $.ajax({
-            url: "../../../controller/user.ajax.php?action=check-message",
-            dataType: "json",
-            type: "GET",
-            success:function(data){
+function ajax(){
+    /** VERIFICATION A LA CONNEXION DE L'UTILISATEUR **/
+    $.ajax({
+        url: "../../../controller/user.ajax.php?action=check-message",
+        dataType: "json",
+        type: "GET",
+        success:function(data){
+            /*if(data >= 1){
+             $('#count_mail').replaceWith('<span id="count_mail" class="badge badge-primary badge-header">'+data+'</span>');
+             $('#count_mail_title').replaceWith('<p class="pull-left" id="count_mail_title">Vous avez '+data+' messages non lu</p>');
+             toastr.info("Vous avez "+data+" Message non lu !", "MESSAGERIE");
+             }else{
+             $('#count_mail').remove();
+             $('#count_mail_title').replaceWith('<p class="pull-left" id="count_mail_title">Aucun nouveau mail</p>');
+             }*/
+            if(check_message() != data){
                 if(data >= 1){
                     $('#count_mail').replaceWith('<span id="count_mail" class="badge badge-primary badge-header">'+data+'</span>');
                     $('#count_mail_title').replaceWith('<p class="pull-left" id="count_mail_title">Vous avez '+data+' messages non lu</p>');
@@ -17,16 +25,29 @@ $(document).ready(function(){
                     $('#count_mail').remove();
                     $('#count_mail_title').replaceWith('<p class="pull-left" id="count_mail_title">Aucun nouveau mail</p>');
                 }
-            },
-            error:function(jqxhr){
-                console.log(jqxhr.responseText);
+            }else{
+                return false;
             }
-        });
-        $.ajax({
-            url: "../../../controller/user.ajax.php?action=check-notif",
-            dataType: "json",
-            type: "GET",
-            success: function(data){
+        },
+        error:function(jqxhr){
+            console.log(jqxhr.responseText);
+        }
+    });
+    $.ajax({
+        url: "../../../controller/user.ajax.php?action=check-notif",
+        dataType: "json",
+        type: "GET",
+        success: function(data){
+            /*if(data >= 1){
+                $('#count_notif').replaceWith('<span id="count_notif" class="badge badge-danger badge-header">'+data+'</span>');
+                $('#count_notif_title').replaceWith('<p class="pull-left" id="count_notif_title">'+data+' Notification en attente</p>');
+                toastr.info("Vous avez une nouvelle notification !");
+            }else{
+                $('#count_notif').remove();
+                $('#count_notif_title').replaceWith('<p class="pull-left" id="count_notif_title">'+data+' Notification en attente</p>');
+
+            }*/
+            if(check_notif() != data){
                 if(data >= 1){
                     $('#count_notif').replaceWith('<span id="count_notif" class="badge badge-danger badge-header">'+data+'</span>');
                     $('#count_notif_title').replaceWith('<p class="pull-left" id="count_notif_title">'+data+' Notification en attente</p>');
@@ -36,12 +57,16 @@ $(document).ready(function(){
                     $('#count_notif_title').replaceWith('<p class="pull-left" id="count_notif_title">'+data+' Notification en attente</p>');
 
                 }
-            },
-            error: function(jqxhr){
-                console.log(jqxhr.responseText);
+            }else{
+                return false;
             }
-        });
-    }
+        },
+        error: function(jqxhr){
+            console.log(jqxhr.responseText);
+        }
+    });
+}
+$(document).ready(function(){
     setInterval(function(){
         ajax()
     }, 30000);
