dfc8f16d52d4fd916ebd56e84fbb7b55dcd9d3e6
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-04 15:35:04 +0100

    GESTCOM V1.0.0

--- assets/plugins/custom/js/pages/calendar.js
+++ assets/plugins/custom/js/pages/calendar.js
@@ -6,10 +6,42 @@
     function fnFormatDetails(oTable, nTr) {
         var aData = oTable.fnGetData(nTr);
         var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
-        sOut += '<tr><td>Rendering engine:</td><td>' + aData[1] + ' ' + aData[4] + '</td></tr>';
-        sOut += '<tr><td>Link to source:</td><td>Could provide a link here</td></tr>';
-        sOut += '<tr><td>Extra info:</td><td>And any further details here (images etc)</td></tr>';
+        sOut += '<tr><td>Lieu:</td><td>' + aData[1] + ' ' + aData[4] + '</td></tr>';
+        sOut += '<tr><td>Description:</td><td>Could provide a link here</td></tr>';
         sOut += '</table>';
         return sOut;
     }
+    var nCloneTh = document.createElement('th');
+    var nCloneTd = document.createElement('td');
+    nCloneTd.innerHTML = '<i class="fa fa-plus-square-o"></i>';
+    nCloneTd.className = "center";
+
+    $('#calendar thead tr').each(function () {
+        this.insertBefore(nCloneTh, this.childNodes[0]);
+    });
+    $('#calendar tbody tr').each(function () {
+        this.insertBefore(nCloneTd.cloneNode(true), this.childNodes[0]);
+    });
+
+    var oTable = $('#calendar').dataTable({
+        "aoColumnDefs": [{
+            "bSortable": false,
+            "aTargets": [0]
+        }],
+        "aaSorting": [
+            [1, 'asc']
+        ]
+    });
+    $(document).on('click', '#calendar tbody td i', function () {
+        var nTr = $(this).parents('tr')[0];
+        if (oTable.fnIsOpen(nTr)) {
+            /* This row is already open - close it */
+            $(this).removeClass().addClass('fa fa-plus-square-o');
+            oTable.fnClose(nTr);
+        } else {
+            /* Open this row */
+            $(this).removeClass().addClass('fa fa-minus-square-o');
+            oTable.fnOpen(nTr, fnFormatDetails(oTable, nTr), 'details');
+        }
+    });
 })(jQuery);
