5c3386a30ef60967dcab06d1c81d4a12972227b3
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-04 14:44:16 +0100

    GESTCOM V1.0.0

--- view/login.php
+++ view/login.php
@@ -40,7 +40,8 @@ use App\general\constante;
                     <input type="password" name="password" class="form-control form-white password" placeholder="Mot de Passe" required>
                     <i class="icon-lock"></i>
                 </div>
-
+                <label>
+                    <input type="checkbox" data-checkbox="icheckbox_square-blue"> Se souvenir de moi</label>
                 <button type="submit" id="submit-form" class="btn btn-lg btn-dark btn-rounded ladda-button" data-style="expand-left" name="action" value="login">Connexion</button>
             </form>
         </div>
