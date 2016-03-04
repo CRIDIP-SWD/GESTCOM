40563e94ad91b657ba13fd4a1d09e2a53e92e0d1
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-04 22:08:05 +0100

    GESTCOM V1.0.0

--- view/calendar.php
+++ view/calendar.php
@@ -89,7 +89,10 @@
                                         foreach($sql_event as $event):
                                             ?>
                                             <tr class="<?php if($event->start_event < time() AND $event->end_event < time()){echo 'danger';} ?> <?php if($event->start_event <= time()-900){echo 'info';} ?>">
-
+                                                <td><?= $date_format->formatage_sequenciel("d", $event->start_event); ?></td>
+                                                <td><?= $date_format->formatage("H:i", $event->start_event); ?> / <?= $date_format->formatage("H:i", $event->end_event); ?></td>
+                                                <td><?= html_entity_decode($event->titre_event); ?></td>
+                                                <td></td>
                                             </tr>
                                         <?php endforeach; ?>
                                         </tbody>
