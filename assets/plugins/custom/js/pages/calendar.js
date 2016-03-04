c42bc9802c39e9f218d78695a253bb90bca15063
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-04 15:40:18 +0100

    GESTCOM V1.0.0

--- assets/plugins/custom/js/pages/calendar.js
+++ assets/plugins/custom/js/pages/calendar.js
@@ -6,8 +6,8 @@
     function fnFormatDetails(oTable, nTr) {
         var aData = oTable.fnGetData(nTr);
         var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
-        sOut += '<tr><td>Lieu:</td><td>' + aData[5] + '</td></tr>';
-        sOut += '<tr><td>Description:</td><td>'+aData[6]+'</td></tr>';
+        sOut += '<tr><td style="width: 50%;">Lieu:</td><td style="width: 50%;">' + aData[5] + '</td></tr>';
+        sOut += '<tr><td style="width: 50%;">Description:</td><td style="width: 50%;">'+aData[6]+'</td></tr>';
         sOut += '</table>';
         return sOut;
     }
