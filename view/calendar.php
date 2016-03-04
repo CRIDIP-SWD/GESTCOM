bb6a6fad52be5cb684f8551b0932a589ec52c5bf
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-04 13:02:40 +0100

    GESTCOM V1.0.0

--- view/calendar.php
+++ view/calendar.php
@@ -1,11 +1,5 @@
 <div class="row">
     <div class="col-md-12">
-        <div id="calendar"></div>
+
     </div>
 </div>
-
-<script src='assets/plugins/moment/moment.min.js'></script>
-<script src='assets/plugins/fullcalendar/fullcalendar.min.js'></script>
-<script src='assets/plugins/fullcalendar/dist/lang-all.js'></script>
-<!--<script src="assets/js/pages/fullcalendar.js"></script>-->
-<script src="assets/plugins/custom/js/pages/calendar.js"></script>
