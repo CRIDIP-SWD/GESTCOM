8c7199e0118f69f13d0f0c04f949d88da8b2b3b2
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-05 01:24:27 +0100

    GESTCOM V1.0.0

--- view/calendar.php
+++ view/calendar.php
@@ -95,7 +95,7 @@
                                                 <td><?= $date_format->formatage("H:i", $event->start_event); ?> / <?= $date_format->formatage("H:i", $event->end_event); ?></td>
                                                 <td><?= html_entity_decode($event->titre_event); ?></td>
                                                 <td>
-                                                    <button type="button" class="btn btn-sm btn-icon btn-rounded btn-danger"><i class="fa fa-trash"></i></button>
+                                                    <button class="btn btn-sm btn-icon btn-rounded btn-danger" type="button"><i class="fa fa-google-plus"></i></button>
                                                 </td>
                                             </tr>
                                         <?php endforeach; ?>
