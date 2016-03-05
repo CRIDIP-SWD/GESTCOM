005caebe76429b5b92a5bfa7b255a3cc0787620f
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-05 01:39:37 +0100

    GESTCOM V1.0.0

--- view/calendar.php
+++ view/calendar.php
@@ -95,7 +95,7 @@
                                                 <td><?= $date_format->formatage("H:i", $event->start_event); ?> / <?= $date_format->formatage("H:i", $event->end_event); ?></td>
                                                 <td><?= html_entity_decode($event->titre_event); ?></td>
                                                 <td>
-                                                    <button class="btn btn-sm btn-icon btn-rounded btn-danger" type="button"><i class="fa fa-trash-o"></i></button>
+                                                    <a class="btn btn-sm btn-rounded btn-danger" href="controller/calendar.ajax.php?action=supp-event&iduser=<?= $event->iduser; ?>"><i class="fa fa-trash-o"></i></a>
                                                 </td>
                                             </tr>
                                         <?php endforeach; ?>
@@ -144,7 +144,7 @@
                                                 <td><?= $date_format->formatage("H:i", $event->start_event); ?> / <?= $date_format->formatage("H:i", $event->end_event); ?></td>
                                                 <td><?= html_entity_decode($event->titre_event); ?></td>
                                                 <td>
-                                                    <button class="btn btn-sm btn-icon btn-rounded btn-danger" type="button"><i class="fa fa-trash-o"></i></button>
+                                                    <a class="btn btn-sm btn-rounded btn-danger" href="controller/calendar.ajax.php?action=supp-event&iduser=<?= $event->iduser; ?>"><i class="fa fa-trash-o"></i></a>
                                                 </td>
                                             </tr>
                                         <?php endforeach; ?>
