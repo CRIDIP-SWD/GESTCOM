553c51cd7df1cc9312298d4c65b24bdb8cae5b0a
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-05 01:38:45 +0100

    GESTCOM V1.0.0

--- controller/calendar.ajax.php
+++ controller/calendar.ajax.php
@@ -1,13 +1,10 @@
 <?php
-/*function is_ajax(){
+function is_ajax(){
     return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
 }
 if(is_ajax()){
-    if(isset($_GET['action']) && $_GET['action'] == 'supp-notif')
+    if(isset($_GET['action']) && $_GET['action'] == 'supp-event')
     {
-        require "../application/classe.php";
-        $notif_purge = $DB->execute("DELETE FROM notif");
-
-        echo json_encode($notif_purge);
+        var_dump($_GET);
     }
-}*/
+}
