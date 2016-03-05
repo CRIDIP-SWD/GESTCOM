1d3408a3694de1f167b9722e88689963599e13f2
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-06 00:13:37 +0100

    GESTCOM V1.0.0

--- view/mailbox.php
+++ view/mailbox.php
@@ -1,4 +1,4 @@
 
 
-<script src="<?= $constante->getUrl(array('plugins')); ?>summernote/summernote.min.js"></script>
-<script src="<?= $constante->getUrl(array('plugins')); ?>custom/js/pages/mailbox.js"></script>
+<script src="<?= $constante->getUrl(array('plugins/')); ?>summernote/summernote.min.js"></script>
+<script src="<?= $constante->getUrl(array('plugins/')); ?>custom/js/pages/mailbox.js"></script>
