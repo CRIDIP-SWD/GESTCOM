94d6ae54284e7c5be1ee229bd75a5a68ad6a4b5c
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-08 15:26:32 +0100

    GESTCOM V1.0.0

---
+++ view/gestion/dashboard.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-md-9"></div>
+    <div class="col-md-3">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="jquery-clock small" data-jquery-clock="">
+                    <li class="jquery-clock-pin"></li>
+                    <li class="jquery-clock-sec"></li>
+                    <li class="jquery-clock-min"></li>
+                    <li class="jquery-clock-hour"></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
