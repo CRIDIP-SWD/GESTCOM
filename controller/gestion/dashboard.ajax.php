4ed7db160de7645bb88848543df7eb0527bca53d
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-08 16:12:30 +0100

    GESTCOM V1.0.0

--- controller/gestion/dashboard.ajax.php
+++ controller/gestion/dashboard.ajax.php
@@ -3,5 +3,13 @@ function is_ajax(){
     return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
 }
 if(is_ajax()){
+    if(isset($_GET['action']) && $_GET['action'] == 'count_client')
+    {
+        session_start();
+        require "../../application/classe.php";
 
+        $count_c = $DB->count("SELECT COUNT(idclient) FROM client");
+
+        echo json_encode($count_c);
+    }
 }
