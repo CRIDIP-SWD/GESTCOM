a5a749796d6765ea8b50cd4d1f25b50be1e99dc0
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-08 17:26:23 +0100

    GESTCOM V1.0.0

---
+++ view/gestion/client.php
@@ -0,0 +1,16 @@
+<?php if(!isset($_GET['sub'])): ?>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="well">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1><i class="fa fa-users"></i> CLIENT</h1>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="btn btn-rounded btn-lg btn-primary pull-right" href="index.php?view=mailbox"><i class="fa fa-arrow-circle-left fa-2x"></i> Retour</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
