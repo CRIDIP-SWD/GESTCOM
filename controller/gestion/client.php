3430a27849cda2ccce9031669ed399a180cb6ecc
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-08 19:20:22 +0100

    GESTCOM V1.0.0

--- controller/gestion/client.php
+++ controller/gestion/client.php
@@ -48,11 +48,14 @@ if(isset($_POST['action']) && $_POST['action'] == 'add_client')
     ));
 
     if($client_i == 1 AND $user_client_insert == 1){
-        $fonction->redirect("client", "", "", "success", "add_user", "Le client <strong>$nom_client $prenom_client</strong> à bien été créer");
+        $text = "Le client à été créer avec succès !";
+        header("Location: ../../view/index.php?view=client&sub=view&num_client=$num_client&success=add_client&text=$text");
     }elseif($client_i == 1 AND $user_client_insert == 0){
-        $fonction->redirect("client", "", "", "warning", "add_user", "Le client <strong>$nom_client $prenom_client</strong> à bien été créer mais son groupement de login/password n'à pas été définie !");
+        $text = "Le client <strong>$nom_client $prenom_client</strong> à bien été créer mais son groupement de login/password n'à pas été définie !";
+        header("Location: ../../view/index.php?view=client&sub=view&num_client=$num_client&warning=add_client&text=$text");
     }elseif($client_i == 0 AND $user_client_insert == 1){
-        $fonction->redirect("client", "", "", "warning", "add_user", "Le client <strong>$nom_client $prenom_client</strong> n'à pas été créer mais sont groupement login/password à été définie !");
+        $text = "Le client <strong>$nom_client $prenom_client</strong> n'à pas été créer mais sont groupement login/password à été définie !";
+        header("Location: ../../view/index.php?view=client&sub=view&num_client=$num_client&warning=add_client&text=$text");
     }else{
         var_dump($client_i, $user_client_insert);
     }
