93c557547809754731ba8bd59a3901c84792fd13
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-08 17:34:58 +0100

    GESTCOM V1.0.0

--- view/gestion/client.php
+++ view/gestion/client.php
@@ -11,4 +11,50 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel">
+                <div class="panel-header">
+                    <h3>Liste des Clients</h3>
+                </div>
+                <div class="panel-content">
+                    <div class="table-responsive">
+                        <table class="table table-hover dataTable" id="client">
+                            <thead>
+                                <tr>
+                                    <th>Numéro Client</th>
+                                    <th>Identité</th>
+                                    <th>Adresse</th>
+                                    <th>Coordonnée</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            $sql_client = $DB->query("SELECT * FROM client ORDER BY nom_client ASC");
+                            foreach($sql_client as $client):
+                            ?>
+                                <tr>
+                                    <td><?= $client->num_client; ?></td>
+                                    <td><?= $client->nom_client; ?> <?= $client->prenom_client; ?></td>
+                                    <td>
+                                        <?= html_entity_decode($client->adresse_client); ?><br>
+                                        <?= $client->code_postal; ?> <?= html_entity_decode($client->ville_client); ?>
+                                    </td>
+                                    <td>
+                                        <strong><i class="fa fa-phone-square"></i></strong>: 0<?= $client->num_client; ?><br>
+                                        <strong><i class="fa fa-envelope"></i></strong>: <?= $client->mail_client; ?>
+                                    </td>
+                                    <td>
+
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php endif; ?>
