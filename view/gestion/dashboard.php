aca6d3692de1deba6a9beadcb6f2225616d615d4
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-08 15:29:07 +0100

    GESTCOM V1.0.0

--- view/gestion/dashboard.php
+++ view/gestion/dashboard.php
@@ -13,3 +13,19 @@
         </div>
     </div>
 </div>
+
+<script src="<?= $constante->getUrl(array('plugins/')); ?>noty/jquery.noty.packaged.min.js"></script>  <!-- Notifications -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>bootstrap-editable/js/bootstrap-editable.min.js"></script> <!-- Inline Edition X-editable -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>bootstrap-context-menu/bootstrap-contextmenu.min.js"></script> <!-- Context Menu -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>multidatepicker/multidatespicker.min.js"></script> <!-- Multi dates Picker -->
+<script src="<?= $constante->getUrl(array('js/')); ?>widgets/todo_list.js"></script>
+<script src="<?= $constante->getUrl(array('plugins/')); ?>metrojs/metrojs.min.js"></script> <!-- Flipping Panel -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>charts-chartjs/Chart.min.js"></script>  <!-- ChartJS Chart -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>charts-highstock/js/highstock.min.js"></script> <!-- financial Charts -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>charts-highstock/js/modules/exporting.min.js"></script> <!-- Financial Charts Export Tool -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>maps-amcharts/ammap/ammap.min.js"></script> <!-- Vector Map -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>maps-amcharts/ammap/maps/js/worldLow.min.js"></script> <!-- Vector World Map  -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>maps-amcharts/ammap/themes/black.min.js"></script> <!-- Vector Map Black Theme -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>skycons/skycons.min.js"></script> <!-- Animated Weather Icons -->
+<script src="<?= $constante->getUrl(array('plugins/')); ?>simple-weather/jquery.simpleWeather.js"></script> <!-- Weather Plugin -->
+<script src="<?= $constante->getUrl(array('js/')); ?>widgets/widget_weather.js"></script>
