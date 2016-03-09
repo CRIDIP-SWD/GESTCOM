99aa2dcc229d161f544c2d44f604b8fa20d0dead
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-09 19:53:14 +0100

    push

--- controller/gestion/client.php
+++ controller/gestion/client.php
@@ -62,6 +62,66 @@ if(isset($_POST['action']) && $_POST['action'] == 'add_client')
     <head>
         <meta charset="UTF-8">
         <title>Document</title>
+        <style type="text/css">
+            @import url(http://fonts.googleapis.com/css?family=Open+Sans);
+            body{overflow: hidden}
+            img{max-width:600px;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic}
+            a{text-decoration:none;border:0;outline:none;color:#bbb}
+            a img{border:none}
+            p{margin-top: 0;margin-bottom: 0;text-align:left;}
+            td,h1,h2,h3{font-family:Helvetica,Arial,sans-serif;font-weight:400}
+            td{text-align:center}
+            body{-webkit-font-smoothing:antialiased;-webkit-text-size-adjust:none;width:100%;height:100%;color:#37302d;background:#fff;font-size:16px}
+            table{border-collapse:collapse!important}
+            .headline{color:#fff;font-size:36px}
+            .force-full-width{width:100%!important}
+            .force-width-80{width:80%!important}
+            .force-width-40{width:50%!important}
+            @media screen {
+                td,h1,h2,h3{font-family:'Open Sans','Helvetica Neue','Arial','sans-serif'!important}
+            }
+            @media only screen and (max-width: 600px) {
+                table[class="w290"]{width:290px!important}
+                table[class="w300"]{width:300px!important}
+                table[class="w320"]{width:480px!important}
+                table[class*="w100p"]{width:100%!important}
+                td[class="w320"]{width:4800px!important}
+                td[class="mobile-center"]{text-align:center!important}
+                td[class*="mobile-padding"]{padding-left:20px!important;padding-right:20px!important;padding-bottom:20px!important}
+                td[class*="mobile-block"]{display:block!important;width:100%!important;text-align:left!important;padding-bottom:20px!important}
+                td[class*="mobile-border"]{border:0!important}
+                td[class*="reveal"]{display:block!important}
+                td[class*="mobile-spacing"]{padding-top:10px!important;padding-bottom:10px!important}
+                *[class*="mobile-hide"]{display:none!important}
+                *[class*="mobile-br"]{font-size:12px!important}
+                td[class*="mobile-w20"]{width:20px!important}
+                img[class*="mobile-w20"]{width:20px!important}
+                img[class*="w320"]{width:250px!important;height:67px!important}
+                .mobile-padding {padding:10px 30px !important;;}
+            }
+            @media only screen and (max-width: 480px) {
+                table[class="w290"]{width:290px!important}
+                table[class="w300"]{width:300px!important}
+                table[class="w320"]{width:300px!important}
+                table[class*="w100p"]{width:100%!important}
+                td[class="w320"]{width:300px!important}
+                td[class="mobile-center"]{text-align:center!important}
+                td[class*="mobile-padding"]{padding-left:20px!important;padding-right:20px!important;padding-bottom:20px!important}
+                td[class*="mobile-block"]{display:block!important;width:100%!important;text-align:left!important;padding-bottom:20px!important}
+                td[class*="mobile-border"]{border:0!important}
+                td[class*="reveal"]{display:block!important}
+                td[class*="mobile-spacing"]{padding-top:10px!important;padding-bottom:10px!important}
+                *[class*="mobile-hide"]{display:none!important}
+                *[class*="mobile-br"]{font-size:12px!important}
+                td[class*="mobile-w20"]{width:20px!important}
+                img[class*="mobile-w20"]{width:20px!important}
+                img[class*="w320"]{width:250px!important;height:67px!important}
+                td[class*="activate-now"]{padding-right:0!important;padding-top:20px!important}
+                td[class*="mobile-align"]{text-align:left!important}
+                td[class*="mobile-center"]{text-align:center!important}
+                .mobile-padding {padding:10px 10px !important;}
+            }
+        </style>
     </head>
     <body  offset="0" class="body" style="padding:0; margin:0; display:block; background:#eeebeb; -webkit-text-size-adjust:none" bgcolor="#eeebeb">
     <table align="center" cellpadding="0" cellspacing="0" width="100%" height="100%" >
