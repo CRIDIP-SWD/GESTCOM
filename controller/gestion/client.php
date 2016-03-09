e2474f714891d8cb477602c108c0441afce7a927
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-09 19:52:02 +0100

    push

--- controller/gestion/client.php
+++ controller/gestion/client.php
@@ -63,85 +63,165 @@ if(isset($_POST['action']) && $_POST['action'] == 'add_client')
         <meta charset="UTF-8">
         <title>Document</title>
     </head>
-    <body>
-    <table cellspacing="0" cellpadding="0" width="600" class="w320" style="border-radius: 4px;overflow: hidden;">
+    <body  offset="0" class="body" style="padding:0; margin:0; display:block; background:#eeebeb; -webkit-text-size-adjust:none" bgcolor="#eeebeb">
+    <table align="center" cellpadding="0" cellspacing="0" width="100%" height="100%" >
         <tr>
-            <td align="center" valign="top">
-                <table cellspacing="0" cellpadding="0" class="force-full-width">
-                    <tr>
-                        <td class="bg bg1" style="background-color:#F1F2F5;">
-                            <table cellspacing="0" cellpadding="0" class="force-full-width">
-                                <tr>
-                                    <td style="font-size:24px; font-weight: 600; color: #121212; text-align:center;" class="mobile-spacing">
-                                        <div class="mobile-br">&nbsp;</div>
-                                        <span319db5>Welcome to Themes Lab</span>
-                                            <br>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="font-size:17px; text-align:center; padding: 10px 75px 0; color:#6E6E6E;" class="w320 mobile-spacing mobile-padding">
-                                        <span319db5>We are happy to meet you and hope you have an amazing time with us.</span><br><br>
-                                    </td>
-                                </tr>
-                            </table>
-                            <table cellspacing="0" cellpadding="0" width="100%" style="background-color:#F1F2F5">
-                                <tr>
-                                    <td>
-                                        <img src="images/phone-mockup2.png" style="max-width:100%; display:block;">
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
-                <table cellspacing="0" cellpadding="0" class="force-full-width">
-                    <tr>
-                        <td class="bg bg1" style="background-color:#F1F2F5;">
-                            <center>
-                                <table style="margin: 0 auto" cellpadding="0" cellspacing="0" class="force-width-80">
+            <td align="center" valign="top" style="background-image: url(images/background/13.jpg);background-size: auto 100%;background-position: top center;background-repeat:no-repeat" width="100%">
+                <center>
+                    <table style="margin:0 auto;" cellspacing="0" height="60" cellpadding="0" width="100%">
+                        <tr>
+                            <td style="text-align: center;">
+                                <a href="#"><img width="91" height="28" src="images/logo-white.png" alt="company logo" /></a>
+                            </td>
+                        </tr>
+                    </table>
+                    <table cellspacing="0" cellpadding="0" width="600" class="w320" style="border-radius: 4px;overflow: hidden;">
+                        <tr>
+                            <td align="center" valign="top">
+                                <table cellspacing="0" cellpadding="0" class="force-full-width">
                                     <tr>
-                                        <br><br>
-                                        <td class="mobile-resize" style="color:#172838; font-size: 20px; font-weight: 600; text-align: left; vertical-align: top;">
-                                            <span319db5>Activate your account now</span>
+                                        <td class="bg bg1" style="background-color:#F1F2F5;">
+                                            <table cellspacing="0" cellpadding="0" class="force-full-width">
+                                                <tr>
+                                                    <td style="font-size:24px; font-weight: 600; color: #121212; text-align:center;" class="mobile-spacing">
+                                                        <div class="mobile-br">&nbsp;</div>
+                                                        <span319db5>Welcome to Themes Lab</span>
+                                                            <br>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="font-size:17px; text-align:center; padding: 10px 75px 0; color:#6E6E6E;" class="w320 mobile-spacing mobile-padding">
+                                                        <span319db5>We are happy to meet you and hope you have an amazing time with us.</span><br><br>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                            <table cellspacing="0" cellpadding="0" width="100%" style="background-color:#F1F2F5">
+                                                <tr>
+                                                    <td>
+                                                        <img src="images/phone-mockup2.png" style="max-width:100%; display:block;">
+                                                    </td>
+                                                </tr>
+                                            </table>
                                         </td>
                                     </tr>
                                 </table>
-                                <table style="margin: 0 auto;" cellspacing="0" cellpadding="0" class="force-width-80">
+                                <table cellspacing="0" cellpadding="0" class="force-full-width">
                                     <tr>
-                                        <td style="text-align:left; color: #6f6f6f;">
-                                            <br>
-                                            <p319db5>
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>Sed do eiusmod tempor ullamco laboris.
-                                                Quis nostrud exercitation nisi ut aliquip ex ea commodo!
-                                                </p>
+                                        <td class="bg bg1" style="background-color:#F1F2F5;">
+                                            <center>
+                                                <table style="margin: 0 auto" cellpadding="0" cellspacing="0" class="force-width-80">
+                                                    <tr>
+                                                        <br><br>
+                                                        <td class="mobile-resize" style="color:#172838; font-size: 20px; font-weight: 600; text-align: left; vertical-align: top;">
+                                                            <span319db5>Activate your account now</span>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                                <table style="margin: 0 auto;" cellspacing="0" cellpadding="0" class="force-width-80">
+                                                    <tr>
+                                                        <td style="text-align:left; color: #6f6f6f;">
+                                                            <br>
+                                                            <p319db5>
+                                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>Sed do eiusmod tempor ullamco laboris.
+                                                                Quis nostrud exercitation nisi ut aliquip ex ea commodo!
+                                                                </p>
+                                                    </tr>
+                                                </table>
+                                            </center>
+                                            <table style="margin:0 auto;" cellspacing="0" cellpadding="10" width="100%">
+                                                <tr>
+                                                    <td style="text-align:center; margin:0 auto;">
+                                                        <br>
+                                                        <div>
+                                                            <!--[if mso]>
+                                                            <v:rect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://" style="height:45px;v-text-anchor:middle;width:240px;" stroke="f" fillcolor="#f5774e">
+                                                                <w:anchorlock/>
+                                                                <center>
+                                                            <![endif]-->
+                                                            <a class="btn" href="#"
+                                                               style="background-color:#172838;color:#ffffff;display:inline-block;font-family:'Source Sans Pro', Helvetica, Arial, sans-serif;font-size:18px;font-weight:400;line-height:45px;text-align:center;text-decoration:none;width:240px;-webkit-text-size-adjust:none;
+                                    -webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;">Confirm Email Address</a>
+                                                            <!--[if mso]>
+                                                            </center>
+                                                            </v:rect>
+                                                            <![endif]-->
+                                                        </div>
+                                                        <br>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
                                     </tr>
                                 </table>
-                            </center>
-                            <table style="margin:0 auto;" cellspacing="0" cellpadding="10" width="100%">
-                                <tr>
-                                    <td style="text-align:center; margin:0 auto;">
-                                        <br>
-                                        <div>
-                                            <!--[if mso]>
-                                            <v:rect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://" style="height:45px;v-text-anchor:middle;width:240px;" stroke="f" fillcolor="#f5774e">
-                                                <w:anchorlock/>
-                                                <center>
-                                            <![endif]-->
-                                            <a class="btn" href="#"
-                                               style="background-color:#172838;color:#ffffff;display:inline-block;font-family:'Source Sans Pro', Helvetica, Arial, sans-serif;font-size:18px;font-weight:400;line-height:45px;text-align:center;text-decoration:none;width:240px;-webkit-text-size-adjust:none;
-                                    -webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;">Confirm Email Address</a>
-                                            <!--[if mso]>
-                                            </center>
-                                            </v:rect>
-                                            <![endif]-->
-                                        </div>
-                                        <br>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
+                            </td>
+                        </tr>
+                    </table>
+                    <table cellspacing="0" cellpadding="0" width="600" class="w320" style="border-radius: 4px;overflow: hidden;">
+                        <tr>
+                            <td align="center" valign="top">
+                                <table cellspacing="0" cellpadding="0" class="force-full-80" style="width:80%;margin:auto">
+                                    <tr>
+                                        <td style="text-align:center;">
+                                            &nbsp;
+                                    </tr>
+                                    <tr>
+                                        <td style="color:#D2D2D2;;color:rgba(255,255,255,0.7); font-size: 14px;padding-bottom:4px;">
+                                            <table border="0" align="left" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="force-width-50 w100p">
+                                                <tr>
+                                                    <td style="text-decoration:underline;height:30px;text-align:left" class="mobile-center">
+                                                        <span319db5>Update subscription preferences</span>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                            <table border="0" align="right" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="force-width-50 w100p">
+                                                <tr>
+                                                    <td style="text-decoration:underline;height:30px;text-align:right" class="mobile-center">
+                                                        <span319db5>Unsubscribe from this list</span>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                                <table cellspacing="0" cellpadding="0" class="force-full-80" style="width:80%;margin:auto">
+                                    <tr>
+                                        <td style="text-align:center;">
+                                            &nbsp;
+                                    </tr>
+                                    <tr>
+                                        <td style="color:#D2D2D2;color:rgba(255,255,255,0.5); font-size: 14px;padding-bottom:4px;">
+                                            <table border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="force-width-50">
+                                                <tr>
+                                                    <td style="height:21px;text-align:center;font-size:12px;" class="mobile-center">
+                                                        <span319db5>Copyright © 2015 Your Company, All Right Reserved.</span>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="height:21px;text-align:center;font-size:12px;" class="mobile-center">
+                                                        <span319db5>795 Folsom Avenue, Suite 600. San Francisco, CA 94107, United State</span>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="font-size:12px;">
+                                            &nbsp;
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                    <table cellspacing="0" cellpadding="0" class="force-full-width">
+                        <tr>
+                            <td style="font-size:12px;">
+                                &nbsp;
+                                <br>
+                            </td>
+                        </tr>
+                    </table>
+                </center>
             </td>
         </tr>
     </table>
